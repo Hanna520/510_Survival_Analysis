ec2c3838e5088b964722863414f70d79937c3c3a
hanna <holu@chapman.edu>
2020-10-24 21:42:50 -0700

    almost complete

--- Data_Preprocessing.R
+++ Data_Preprocessing.R
@@ -10,7 +10,13 @@
 library(dplyr)
 library(ggplot2)
 library(stringr)
+library(reshape2)
+library(comorbidity)
+library(survival)
 
+########################################################################
+########################## IMPORT NOTEEVENTS ###########################
+########################################################################
 # Import Noteevents:
 filename = gzfile('NOTEEVENTS.csv.gz','rt')
 note = read.csv(filename)
@@ -22,13 +28,19 @@ num_reports = note %>%
   summarise(NUM_REPORTS = n(),.groups = 'drop') %>%
   select(HADM_ID, NUM_REPORTS)
 
-# Import Addimissions:
+########################################################################
+########################## IMPORT ADMISSIONS ###########################
+########################################################################
+# Import Admissions:
 filename = gzfile('ADMISSIONS.csv.gz','rt')
 admissions = read.csv(filename)
 
 # Merge admissions and num_reports (left join):
 adm = merge(admissions, num_reports, by='HADM_ID', all.x = T)
 
+########################################################################
+########################## IMPORT ICUSTAYS #############################
+########################################################################
 # Import ICU Stays:
 filename = gzfile('ICUSTAYS.csv.gz','rt')
 icu = read.csv(filename)
@@ -45,6 +57,9 @@ icu_last = icu %>%
 # Merge ICU Unit data with admission data:
 adm_icu = merge(adm, icu_last, by = 'HADM_ID', all.x = T)
 
+########################################################################
+########################## IMPORT PATIENTS #############################
+########################################################################
 # Import Patients:
 filename = gzfile('PATIENTS.csv.gz','rt')
 patients = read.csv(filename)
@@ -134,7 +149,7 @@ df4=within(df_adults,{
   })
 
 ########################################################################
-###################### SAVE THE DATASET FOR NOW ########################
+######################### SAVE THE DATASET #############################
 ########################################################################
 
 # Save the dataset and then Drop columns not needed for model:
@@ -149,76 +164,189 @@ df4 = df4 %>%
 
 
 
-# Get ICD Category:
-# Import ICD Diagnoses:
-filename = 'C:\\Users\\520ha\\Desktop\\Chapman\\Research_2\\Medical Notes\\D_ICD_DIAGNOSES.csv.gz'
-f = gzip.open(filename)
-with gzip.open(filename) as f:
-  diagnoses_code = pd.read_csv(f, low_memory = True)
-
-# Recode icd codes by taking the first 3 characters except for Supplemental:
-diagnoses_code['icd_coded'] = [1000 if (x.startswith('V')) or (x.startswith('E')) 
-                               else int(x[:3]) for  x in diagnoses_code['ICD9_CODE']]
-
-# Group new icd codes into major categories:
-diagnoses_code['icd_category'] = ['INFECTIOUS AND PARASITIC DISEASES' if (x>0 and x <= 139) 
-  else 'NEOPLASMS' if x <= 239 
-  else 'ENDOCRINE, NUTRITIONAL AND METABOLIC DISEASES, AND IMMUNITY DISORDERS' if x<=279 
-  else 'DISEASES OF BLOOD AND BLOOD-FORMING ORGANS' if x<=289
-  else 'MENTAL DISORDERS' if x<=319
-  else 'DISEASES OF THE NERVOUS SYSTEM AND SENSE ORGANS' if x<=389
-  else 'DISEASES OF THE CIRCULATORY SYSTEM' if x<= 459
-  else 'DISEASES OF THE RESPIRATORY SYSTEM' if x<= 519
-  else 'DISEASES OF THE DIGESTIVE SYSTEM' if x<= 579
-  else 'DISEASES OF THE GENITOURINARY SYSTEM' if x<= 629
-  else 'COMPLICATIONS OF PREGNANCY, CHILDBIRTH, AND THE PUERPERIUM' if x<= 679
-  else 'DISEASES OF THE SKIN AND SUBCUTANEOUS TISSUE' if x<= 709
-  else 'DISEASES OF THE MUSCULOSKELETAL SYSTEM AND CONNECTIVE TISSUE' if x<= 739
-  else 'CONGENITAL ANOMALIES' if x <= 759
-  else 'OTHER OR MISSING' if x <= 799
-  else 'INJURY AND POISONING' if x <= 999
-  else 'OTHER OR MISSING' for x in diagnoses_code['icd_coded']]
-
-# Get a list of unique icd_coded and its corresponding category:
-icd_category = diagnoses_code.groupby(['icd_coded']).first()['icd_category']
-code = icd_category.index
-category = list(icd_category.values)
-
-# Make the list into a data frame:
-code_dict = {'icd_coded': [str(x) for x in code], 'category': category}
-icd_cat = pd.DataFrame(code_dict)
+########################################################################
+######################## IMPORT DIAGNOSES_ICD ##########################
+########################################################################
 
 # Import patient diagnoses data:
-filename = 'C:\\Users\\520ha\\Desktop\\Chapman\\Research_2\\Medical Notes\\DIAGNOSES_ICD.csv.gz'
-f = gzip.open(filename)
-with gzip.open(filename) as f:
-  diagnoses = pd.read_csv(f, low_memory = True)
+filename = gzfile('DIAGNOSES_ICD.csv.gz','rt')
+diagnoses = read.csv(filename)
+
+diagnoses = diagnoses[,c("HADM_ID","ICD9_CODE")]
+
+df_com = merge(df4, diagnoses, by = 'HADM_ID', all.x = T)
+
+########################################################################
+########################## COMORBIDITY SCORES ##########################
+########################################################################
+
+# Charlson score based on ICD-10 diagnostic codes:
+df_comorbidity = comorbidity(x = df_com, id = "HADM_ID", code = "ICD9_CODE", 
+                             score = "charlson", assign0 = FALSE, icd = 'icd9')
+
+# Remove the last four items from the comorbidity output
+df_comorbidity1 = df_comorbidity[,!names(df_comorbidity) %in% 
+                                   c("score", "index","wscore","windex")]
+
+# Or keep only wscore from the comorbidity output
+df_comorbidity2 = subset(df_comorbidity, select = c(HADM_ID,wscore))
+
+# Remove the icd code column from original data frame:
+com_no_icd = df_com[,names(df_com) != 'ICD9_CODE']
+
+# Remove duplicates from original data frame:
+unique_com = unique(com_no_icd)
+
+# Merge the main dataset and the comorbidity:
+com_all = merge(unique_com, df_comorbidity1, by="HADM_ID")
+# com_all2= merge(unique_com, df_comorbidity2, by="HADM_ID")
+
+# Remove "INSURANCE":
+com_all <- com_all[, !names(com_all) %in% c('INSURANCE','INSURANCE_CAT')]
+
+# Take only those who were sent home:
+home = com_all[com_all$DISCH_LOC_CAT=='HOME',]
+home = home[,names(home) != 'DISCH_LOC_CAT']
+
+# Import ventilation information and join with the home dataset
+# vent = read.csv('ventilation.csv')
+# vent = select(vent, -"X")
+# home_vent = left_join(home,vent,by='HADM_ID')
+
+########################################################################
+########################### SURVIVAL ANALYSIS ##########################
+########################################################################
+
+# Remove insignificant variables:
+home=select(home,-c(diab,dementia,cevd,pud))
+home=select(home,-c(rheumd,diabwc))
+home=select(home,-ami)
+home=select(home,-LANGUAGE_CATEGORY)
+home=select(home,-GENDER)
+
+# home_vent=select(home_vent,-c(GENDER,diab,dementia,cevd,pud,rheumd,diabwc,ami))
+
+
+# For Survival Analysis without ventilation:
+time = home$Duration
+event = home$EVENT
+x <- subset(home, select = -c(HADM_ID, Duration, EVENT))
+dim(x)
+
+# kp = survfit(Surv(time, event) ~1, data=dataname)
+
+# Survival Analysis with ventilation
+# time = home_vent$Duration
+# event = home_vent$EVENT
+# # x <- subset(home, select = -c(HADM_ID, Duration, EVENT, wscore))
+# x <- subset(home_vent, select = -c(HADM_ID, Duration, EVENT))
+# dim(x)
+
+coxph = coxph(Surv(time,event) ~ ., data = x)
+summary(coxph)
+
+# reg=survreg(Surv(time,event) ~ ., data=x, dist="lognormal")
+# summary(reg)
+
+# Plot
+install.packages('survminer')
+library(survminer)
+plt = ggforest(coxph, data = home)
+
+# Save plot:
+tiff(file="saving_plot3.tiff",
+     width=8, height=10, units="in", res=300)
+ggforest(coxph, data = home, main="Hazard Ratio for Patients Discharged Home")
+dev.off()
+
+# Save model output:
+sink(file = 'cox_cmbdty_Home.txt')
+summary(coxph)
+sink(file=NULL)
+
+
+####################################################
+kp = survfit(Surv(time, event) ~as.factor(AGE_AT_DISCH_3), data=home)
+# plot(kp,xlab="Time (days)",xlim=c(0,90),ylab="Survival probability",
+#      col=c(1,2,3,4),conf.int=F, main="Kaplan Meier Graph")
+
+# Need to find a way to extract the number of observations from diff
+diff = survdiff(Surv(time, event)~AGE_AT_DISCH_3+ADMISSION_TYPE, data= home)
+
+
+survdiff(Surv(time, event)~AGE_AT_DISCH_3+ADMISSION_TYPE, data= home)
+require(stringr)
+require(tidyr)
+nn=str_extract(diff$n, "{1,4}")
+
+
+########################################################################
+# The following are for Num_Diagnoses. Not finished. ###################
+########################################################################
+
+########################################################################
+######################## IMPORT D_ICD_DIAGNOSES ########################
+########################################################################
+# Import ICD Diagnoses:
+filename = gzfile('D_ICD_DIAGNOSES.csv.gz','rt')
+diagnoses_code = read.csv(filename)
+
+# Get ICD Category:
+# Recode icd codes by taking the first 3 characters except for Supplemental:
+diagnoses_code = diagnoses_code %>%
+  mutate(icd_coded = ifelse(substr(ICD9_CODE,1,1) %in% c('V','E'), 1000,
+                            substr(ICD9_CODE,1,3))) %>%
+  # Group new icd codes into major categories:
+  mutate(icd_category = cut(as.numeric(icd_coded), 
+                            breaks = c(0,139,239,279,289,319,389,459,519,579,
+                                       629,679,709,739,759,779,799,999,Inf),
+                            labels = c('INFECTIOUS AND PARASITIC DISEASES',
+                                       'NEOPLASMS',
+                                       'ENDOCRINE, NUTRITIONAL AND METABOLIC DISEASES, AND IMMUNITY DISORDERS',
+                                       'DISEASES OF BLOOD AND BLOOD-FORMING ORGANS',
+                                       'MENTAL DISORDERS',
+                                       'DISEASES OF THE NERVOUS SYSTEM AND SENSE ORGANS',
+                                       'DISEASES OF THE CIRCULATORY SYSTEM',
+                                       'DISEASES OF THE RESPIRATORY SYSTEM',
+                                       'DISEASES OF THE DIGESTIVE SYSTEM',
+                                       'DISEASES OF THE GENITOURINARY SYSTEM',
+                                       'COMPLICATIONS OF PREGNANCY, CHILDBIRTH, AND THE PUERPERIUM',
+                                       'DISEASES OF THE SKIN AND SUBCUTANEOUS TISSUE',
+                                       'DISEASES OF THE MUSCULOSKELETAL SYSTEM AND CONNECTIVE TISSUE',
+                                       'CONGENITAL ANOMALIES',
+                                       'CERTAIN CONDITIONS ORIGINATING IN THE PERINATAL PERIOD',
+                                       'SYMPTOMS, SIGNS, AND ILL-DEFINED CONDITIONS',
+                                       'INJURY AND POISONING',
+                                       'SUPPLEMENTAL OR MISSING')))
+
+# Get a list of unique icd_coded and its corresponding category:
+category = diagnoses_code %>%
+  select(icd_coded, icd_category) %>%
+  group_by(icd_coded) %>%
+  distinct()
+
 
 # Recode the ICD code the same way as earlier:
-diagnoses['icd_coded'] = ['missing' if pd.isnull(x) 
-                          else '1000' if (x.startswith('V')) or (x.startswith('E')) 
-                          else str(int(str(x)[:3])) for  x in diagnoses['ICD9_CODE']]
+# diagnoses = diagnoses %>%
+#   mutate(icd_coded = ifelse(substr(ICD9_CODE,1,1) %in% c('V','E'), 1000,
+#                             substr(ICD9_CODE,1,3)))
+# 
+# # Merge the patient diagnoses code with the code-category list:
+# diag_code = merge(diagnoses, category, by = 'icd_coded')
+# 
+# # Get the number of diagnoses under each major category for each HADM_ID:
+# # Group diag_code by the combination of HADM_ID and category and get the count:
+# count = diag_code %>%
+#   group_by(HADM_ID,icd_category) %>%
+#   mutate(NUM_DIAG = n()) 
+# 
+# # Make icd categories as columns by pivoting count:
+# count_wide = count %>%
+#   select(HADM_ID,icd_category,NUM_DIAG) %>%
+#   dcast(HADM_ID ~ icd_category, value.var = 'NUM_DIAG')
 
-# Merge the patient diagnoses code with the code-category list:
-diag_code = pd.merge(diagnoses, icd_cat, on = 'icd_coded', how = 'inner')
 
-# Get the number of diagnoses under each major category for each HADM_ID:
-# Group diag_code by the combination of HADM_ID and category and get the count:
-count = diag_code.groupby(['HADM_ID','category'])['category'].count()
-count = pd.DataFrame({'count' : count}).reset_index()
 
-# Make icd categories as columns by pivoting count:
-count_wide = count.pivot(columns='category',values='count')
-
-# Get HADM_ID back to the data
-diag_count = pd.concat([count, count_wide],axis=1)
-
-# Replace NAs with 0
-diag_count.fillna(0,inplace=True)
-
-# Remove the duplicated HADM_IDs and comobine the counts for each category into one row:
-diag = diag_count.groupby('HADM_ID').sum().reset_index()
-diag.drop('count',axis=1, inplace=True)
 
 # Merge with the main dataset:
 adults = pd.merge(df_adults, diag, on = 'HADM_ID', how = 'inner')
